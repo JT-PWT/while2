014f5ee5bf5a79d4d0f611755610e1405e6777fb
Jeferson <jeferson.silva220@gmail.com>
2024-09-13 21:30:35 -0300

    Add files via upload

---
+++ senha.js
@@ -0,0 +1,10 @@
+let senha = "malu123";
+
+while (senha <=5) {
+    if (senha !== "malu123"){
+        console.log("Senha incorreta!!!")
+    }else{
+        console.log("Senha correta!!!")
+    }
+    console.log()
+}
---
+++ senha2.js
@@ -0,0 +1,10 @@
+senhaCorreta = "CO.DE"
+senhaCorreta1 = "CO.DE2"
+senhaCorreta2 = "CO.DE3"
+
+senhaUsuario = prompt('Digite a senha:')
+
+while(senhaUsuario !==senhaCorreta && senhaUsuario !== senhaCorreta1 && senhaUsuario !== senhaCorreta2){
+    senhaUsuario = prompt('Digite a senha:')
+}
+
